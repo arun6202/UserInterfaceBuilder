bb4f76b198cce9e87d606536dbe81abd4940a151
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-06 21:30:32 +0530

    compress fix

--- UserInterfaceBuilder/Builder/ImageBuilder.cs
+++ UserInterfaceBuilder/Builder/ImageBuilder.cs
@@ -1,274 +1,202 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using Xamarin.Forms;
 using XamarinFormsStarterKit.UserInterfaceBuilder.Helpers;
 using XamarinFormsStarterKit.UserInterfaceBuilder.UIElements;
+using System.Linq;
+
 
 namespace XamarinFormsStarterKit.UserInterfaceBuilder
 {
-    public static class ImageBuilder
-    {
+	public static class ImageBuilder
+	{
 
-        static readonly List<string> SvgImageList = new List<string>();
+		static readonly List<string> PNGImageList = new List<string>();
 
 
-        static ImageBuilder()
-        {
-            foreach (var item in typeof(ImageBuilder).GetFields())
-            {
-                SvgImageList.Add((string)item.GetValue(new Object()));
+		static ImageBuilder()
+		{
 
-            }
+			var resourceNames = Assembly.GetCallingAssembly().GetManifestResourceNames();
 
-        }
+			var resourcePaths = resourceNames
+				.Where(x => x.Contains("SVG"))
+				.ToArray();
 
-        public static void GenerateImage(Layout layout, bool suppressBackGroundColor = true)
-        {
+			foreach (var resource in resourcePaths)
+			{
+				PNGImageList.Add(resource);
 
-            foreach (var child in layout.Children)
-            {
-                if (child is Layout currentLayout)
-                {
-                    GenerateImage(currentLayout, suppressBackGroundColor);
-                }
+			}
 
-                var currentControl = (VisualElement)child;
+		}
 
-                var imageAttributtes = new Preserver.Image();
-
-                if (suppressBackGroundColor)
-                {
-
-                    imageAttributtes.BackGroundColor = new Preserver.Color(Color.Default);
-                }
-
-                if (child is SVGImage img)
-                {
-                    double height, width;
-                    FinalizeDimensions(img, out height, out width);
-                    imageAttributtes.Source = RandomImage();
-                    imageAttributtes.Height = height;
-                    imageAttributtes.Width = width;
-                    ComponentBuilder.PreserveUIAttributes.Image.Add(imageAttributtes);
-
-                }
-
-            }
-
-
-        }
-
-
-        public static void LoadImage(Layout layout, bool apply = true, bool suppressBackGroundColor = true, bool preserveSession = false)
-        {
-            if (!apply)
-            {
-                return;
-            }
-
-            foreach (var child in layout.Children)
-            {
-                if (child is Layout currentLayout)
-                {
-                    LoadImage(currentLayout, apply, suppressBackGroundColor);
-                }
-
-                var currentControl = (VisualElement)child;
-
-                if (suppressBackGroundColor)
-                {
-
-                    currentControl.BackgroundColor = Color.Transparent;
-                }
-
-                if (child is SVGImage img)
-                {
-
-                    FinalizeDimensions(img, out double height, out double width);
-                    img.Source = RandomImage();
-                    img.HeightRequest = height;
-                    img.WidthRequest = width;
-
-                }
-
-            }
-
-
-        }
-
-
-        private static void FinalizeDimensions(SVGImage img, out double height, out double width)
-        {
-            var source = img.Source.ToString();
-            source = source.Replace("File:", "").ToLower().Trim();
-            height = 40;
-            width = 40;
-            switch (source)
-            {
-                case "fs":
-                    height = 768;
-                    width = 1024;
-                    break;
+		public static void GenerateImage(Layout layout, bool suppressBackGroundColor = true)
+		{
 
-                case "xxxs":
-                    height = 10;
-                    width = 10;
-                    break;
+			foreach (var child in layout.Children)
+			{
+				if (child is Layout currentLayout)
+				{
+					GenerateImage(currentLayout, suppressBackGroundColor);
+				}
 
-                case "xxs":
-                    height = 20;
-                    width = 20;
-                    break;
-                case "xs":
-                    height = 30;
-                    width = 30;
-                    break;
-                case "sm":
-                    height = 40;
-                    width = 40;
-                    break;
-                case "md":
-                    height = 60;
-                    width = 60;
-                    break;
-                case "lg":
-                    height = 80;
-                    width = 80;
-                    break;
-                case "xl":
-                    height = 100;
-                    width = 100;
-                    break;
-                case "xxl":
-                    height = 120;
-                    width = 120;
-                    break;
-                case "xxxl":
-                    height = 450;
-                    width = 350;
-                    break;
+				var currentControl = (VisualElement)child;
 
-                default:
-                    if (source.Trim() != string.Empty)
-                    {
-                        var dimensions = source.Split(new string[] { "/" }, StringSplitOptions.None);
+				var imageAttributtes = new Preserver.Image();
 
-                        if (Int32.TryParse(dimensions[0], out int localwidth))
-                        {
-                            width = localwidth;
-                        }
+				if (suppressBackGroundColor)
+				{
 
-                        if (Int32.TryParse(dimensions[1], out int localheight))
-                        {
-                            height = localheight;
-                        }
-
-                    }
-                    else
-                    {
-                        height = 40;
-                        width = 40;
-
-                    }
-
-                    break;
-            }
-        }
-
-        static string RandomImage()
-        {
-            if (SvgImageList.Count == 0)
-            {
-                return ResourceLoader.GetEmbeddedResourceString("GreenStripes.svg");
-            }
-            var randomIndex = new Random().Next(SvgImageList.Count);
-            var img = SvgImageList[randomIndex];
-            SvgImageList.RemoveAt(randomIndex);
-            return img;
-        }
-
-        public static string HorizontalStripes = "HorizontalStripes.svg";
-
-
-
-        public static string GreenStripes = "GreenStripes.svg";
-
-
-
-        public static string Honeycomb = "Honeycomb.svg";
-
-
-
-        public static string Chevrons = "Chevrons.svg";
-
-        public static string CarbonFiber = "CarbonFiber.svg";
-
-
-
-        public static string MicrobialMat = "MicrobialMat.svg";
-
-
-
-        public static string Dance = "Dance.svg";
-
-
-
-        public static string Checkerboard = "Checkerboard.svg";
-
-
-
-        public static string Waves = "Waves.svg";
-
-
-
-        public static string VerticalStripes = "VerticalStripes.svg";
-
-
-
-        public static string Shippo = "Shippo.svg";
-
-
-
-        public static string HalfRombes = "HalfRombes.svg";
-
-
-
-        public static string Transparent = "Transparent.svg";
-
-
-
-        public static string SimpleHorizontal = "SimpleHorizontal.svg";
-
-
-
-        public static string WhiteCarbon = "WhiteCarbon.svg";
-
-
-
-        public static string CrossStripes = "CrossStripes.svg";
-
-
-
-        public static string SubtleDots = "SubtleDots.svg";
-
-
-
-        public static string ThinStripes = "ThinStripes.svg";
-
-
-
-        public static string SpeckledNoise = "SpeckledNoise.svg";
-
-
-
-        public static string BlueprintGrid = "BlueprintGrid.svg";
-
-
-
-        public static string Argyle = "Argyle.svg";
-
-
-
-
-    }
+					imageAttributtes.BackGroundColor = new Preserver.Color(Color.Default);
+				}
+
+				if (child is SVGImage img)
+				{
+					double height, width;
+					FinalizeDimensions(img, out height, out width);
+					imageAttributtes.Source = RandomImage();
+					imageAttributtes.Height = height;
+					imageAttributtes.Width = width;
+					ComponentBuilder.PreserveUIAttributes.Image.Add(imageAttributtes);
+
+				}
+
+			}
+
+
+		}
+
+
+		public static void LoadImage(Layout layout, bool apply = true, bool suppressBackGroundColor = true, bool preserveSession = false)
+		{
+			if (!apply)
+			{
+				return;
+			}
+
+			foreach (var child in layout.Children)
+			{
+				if (child is Layout currentLayout)
+				{
+					LoadImage(currentLayout, apply, suppressBackGroundColor);
+				}
+
+				var currentControl = (VisualElement)child;
+
+				if (suppressBackGroundColor)
+				{
+
+					currentControl.BackgroundColor = Color.Transparent;
+				}
+
+				if (child is SVGImage img)
+				{
+
+					FinalizeDimensions(img, out double height, out double width);
+					img.Source = RandomImage();
+					img.HeightRequest = height;
+					img.WidthRequest = width;
+
+				}
+
+			}
+
+
+		}
+
+
+		private static void FinalizeDimensions(SVGImage img, out double height, out double width)
+		{
+			var source = img.Source.ToString();
+			source = source.Replace("File:", "").ToLower().Trim();
+			height = 40;
+			width = 40;
+			switch (source)
+			{
+				case "fs":
+					height = 768;
+					width = 1024;
+					break;
+
+				case "xxxs":
+					height = 10;
+					width = 10;
+					break;
+
+				case "xxs":
+					height = 20;
+					width = 20;
+					break;
+				case "xs":
+					height = 30;
+					width = 30;
+					break;
+				case "sm":
+					height = 40;
+					width = 40;
+					break;
+				case "md":
+					height = 60;
+					width = 60;
+					break;
+				case "lg":
+					height = 80;
+					width = 80;
+					break;
+				case "xl":
+					height = 100;
+					width = 100;
+					break;
+				case "xxl":
+					height = 120;
+					width = 120;
+					break;
+				case "xxxl":
+					height = 450;
+					width = 350;
+					break;
+
+				default:
+					if (source.Trim() != string.Empty)
+					{
+						var dimensions = source.Split(new string[] { "/" }, StringSplitOptions.None);
+
+						if (Int32.TryParse(dimensions[0], out int localwidth))
+						{
+							width = localwidth;
+						}
+
+						if (Int32.TryParse(dimensions[1], out int localheight))
+						{
+							height = localheight;
+						}
+
+					}
+					else
+					{
+						height = 40;
+						width = 40;
+
+					}
+
+					break;
+			}
+		}
+
+		static string RandomImage()
+		{
+			if (PNGImageList.Count == 0)
+			{
+				return ResourceLoader.GetEmbeddedResourceString("GreenStripes.svg");
+			}
+			var randomIndex = new Random().Next(PNGImageList.Count);
+			var img = PNGImageList[randomIndex];
+			PNGImageList.RemoveAt(randomIndex);
+			return img;
+		}
+
+
+	}
 }
