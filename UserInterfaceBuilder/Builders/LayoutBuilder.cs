98a4eb1ff84f8d2f84ff0a0a8159ec900dbb8bd6
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-05-29 16:29:06 +0530

    fix

--- UserInterfaceBuilder/Builders/LayoutBuilder.cs
+++ UserInterfaceBuilder/Builders/LayoutBuilder.cs
@@ -41,6 +41,97 @@ namespace XamarinFormsStarterKit.UserInterfaceBuilder
 			}
 		}
 
+		public static void ColorizeLayout(Layout layout, bool apply = true, bool suppressBackGroundColor = true, bool preserveSession = false)
+		{
+			preserveSession = !preserveSession;
+			if (!apply)
+			{
+				return;
+			}
+
+			if (suppressBackGroundColor)
+			{
+
+				layout.BackgroundColor = Color.Default;
+                
+			}
+			else
+			{
+				layout.BackgroundColor = GetColor(preserveSession);
+
+			}
+
+			foreach (var child in layout.Children)
+			{
+				if (child is Layout currentLayout)
+				{
+					ColorizeLayout(currentLayout, apply, suppressBackGroundColor, !preserveSession);
+				}     
+			}
+		}
+
+		public static Color GetColor(bool isRandom = true)
+		{
+			if (isRandom)
+			{
+				if (ColorList.Count == 0)
+				{
+					foreach (var item in typeof(Color).GetFields())
+					{
+						ColorList.Add((Color)item.GetValue(new Color()));
+
+					}
+				}
+				var randomIndex = new Random().Next(ColorList.Count);
+				var color = ColorList[randomIndex];
+				ColorList.RemoveAt(randomIndex);
+				return color;
+			}
+			else
+			{
+				try
+				{
+					if (ComponentBuilder.RestoredUIAttributes.Layout.Any())
+					{
+						var color = ComponentBuilder.RestoredUIAttributes.Layout[0];
+						ComponentBuilder.RestoredUIAttributes.Layout.RemoveAt(0);
+						return color.ToXamarinColor();
+					}
+				}
+				finally
+				{
+
+				}
+			}
+
+			return GetColor(true);
+		}
+        
+		#region  HookTapGestureRecognizer
+
+		public static void CompressLayoutAsHeadless(Layout layout, bool apply = true)
+		{
+			if (!apply)
+			{
+				return;
+			}
+
+			foreach (var child in layout.Children)
+			{
+				if (child is Layout currentLayout)
+				{
+					CompressLayoutAsHeadless(currentLayout);
+				}
+
+				if (child is Layout currentLayoutHeadless)
+				{
+					CompressedLayout.SetIsHeadless(child, apply);
+
+				}
+
+			}
+		}
+
 		internal static void HookTapGestureRecognizer(Layout layout, bool apply)
 		{
 			if (!apply)
@@ -129,93 +220,6 @@ namespace XamarinFormsStarterKit.UserInterfaceBuilder
 			}
 
 		}
-
-		public static void ColorizeLayout(Layout layout, bool apply = true, bool suppressBackGroundColor = true, bool preserveSession = false)
-		{
-			preserveSession = !preserveSession;
-			if (!apply)
-			{
-				return;
-			}
-
-			if (suppressBackGroundColor)
-			{
-				layout.BackgroundColor = Color.Default;
-
-			}
-			else
-			{
-				layout.BackgroundColor = GetColor(preserveSession);
-
-			}
-
-			foreach (var child in layout.Children)
-			{
-				if (child is Layout currentLayout)
-				{
-					ColorizeLayout(currentLayout, apply, suppressBackGroundColor, !preserveSession);
-				}
-			}
-		}
-
-		public static void CompressLayoutAsHeadless(Layout layout, bool apply = true)
-		{
-			if (!apply)
-			{
-				return;
-			}
-
-			foreach (var child in layout.Children)
-			{
-				if (child is Layout currentLayout)
-				{
-					CompressLayoutAsHeadless(currentLayout);
-				}
-
-				if (child is Layout currentLayoutHeadless)
-				{
-					CompressedLayout.SetIsHeadless(child, apply);
-
-				}
-
-			}
-		}
-
-		public static Color GetColor(bool isRandom = true)
-		{
-			if (isRandom)
-			{
-				if (ColorList.Count == 0)
-				{
-					foreach (var item in typeof(Color).GetFields())
-					{
-						ColorList.Add((Color)item.GetValue(new Color()));
-
-					}
-				}
-				var randomIndex = new Random().Next(ColorList.Count);
-				var color = ColorList[randomIndex];
-				ColorList.RemoveAt(randomIndex);
-				return color;
-			}
-			else
-			{
-				try
-				{
-					if (ComponentBuilder.RestoredUIAttributes.Layout.Any())
-					{
-						var color = ComponentBuilder.RestoredUIAttributes.Layout[0];
-						ComponentBuilder.RestoredUIAttributes.Layout.RemoveAt(0);
-						return color.ToXamarinColor();
-					}
-				}
-				finally
-				{
-
-				}
-			}
-
-			return GetColor(true);
-		}
+		#endregion
 	}
 }
