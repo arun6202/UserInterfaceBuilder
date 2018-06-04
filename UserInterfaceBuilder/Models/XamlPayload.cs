627af35802ddff0d3160dc91258008cfd963c172
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-06-04 11:57:55 +0530

    xaml payload

---
+++ UserInterfaceBuilder/Models/XamlPayload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace XamarinFormsStarterKit.UserInterfaceBuilder.Models
+{
+	public class XamlPayload
+	{
+		public XamlPayload()
+		{
+		}
+
+		public string XAML
+		{
+			get;
+			set;
+		}
+
+		public string PreserveXML
+		{
+			get;
+			set;
+		}
+
+	}
+}
