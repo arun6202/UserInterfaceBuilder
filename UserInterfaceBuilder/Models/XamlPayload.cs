8285fa183e58320f396bb153ca0ccefdc0510b23
Arun Balakrishnan <arun.balakrishnan@live.com>
2018-06-05 15:23:37 +0530

    sync

--- UserInterfaceBuilder/Models/XamlPayload.cs
+++ UserInterfaceBuilder/Models/XamlPayload.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 namespace XamarinFormsStarterKit.UserInterfaceBuilder.Models
 {
-	public class XamlPayload
+	public class XamlPayload : IComparable
 	{
 		public XamlPayload()
 		{
@@ -19,5 +19,11 @@ namespace XamarinFormsStarterKit.UserInterfaceBuilder.Models
 			set;
 		}
 
+		public int CompareTo(object obj)
+		{
+			XamlPayload Temp = (XamlPayload)obj;
+
+			return this.XAML.Equals(Temp.XAML) & this.PreserveXML.Equals(Temp.PreserveXML) ? 0 : 1;
+		}
 	}
 }
